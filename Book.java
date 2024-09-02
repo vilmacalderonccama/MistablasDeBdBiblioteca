a9c96e69152ff08527c2d48b4341668d69612347
anavilma <anaestudiante999@gmail.com>
2024-09-02 11:26:18 -0500

    Mis tablas de bdBiblioteca

---
+++ Book.java
@@ -0,0 +1,74 @@
+package models;
+
+
+import java.util.Date;
+
+public class Book {
+    private int id;
+    private String name;
+    private String title;
+    private String publisher;
+    private char isbn;
+    private Date create_at;
+    private Date update_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public char getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(char isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+
+    public Book(){
+
+    }
+}
---
+++ Loan.java
@@ -0,0 +1,71 @@
+package models;
+
+import java.util.Date;
+
+public class Loan {
+    private int id;
+    private User user;  // Se relaciona con la clase User
+    private Book book;  // Se relaciona con la clase Book
+    private Date loanDate;
+    private Date returnDate;
+    private Date createdAt;
+    private Date updatedAt;
+
+    // Getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
